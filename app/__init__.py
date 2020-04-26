5757510d11f253a91629d501459d44a0ea5928a1
GiselleTodd <43821673+GiselleTodd@users.noreply.github.com>
2020-04-26 14:28:44 -0500

    Change URI to heroku postgres

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SUBJECTTTT2CHA@NGE"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:vampirelove98@localhost/project1"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = ""
 app.config['UPLOAD_FOLDER'] = './app/static/images'
 
 db = SQLAlchemy(app)
