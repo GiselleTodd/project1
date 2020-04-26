6025eee684d2c68da1082d22f55aaa77ef27eb81
GiselleTodd <43821673+GiselleTodd@users.noreply.github.com>
2020-04-26 14:42:37 -0500

    Add database URI

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "SUBJECTTTT2CHA@NGE"
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://rfgrorretkdxcd:1a882fb4cef3e67c2ab85f24c906434832190334004b9b111ae86ee6da82b7ec@ec2-34-225-82-212.compute-1.amazonaws.com:5432/d7ru2pa69qs41"
 app.config['UPLOAD_FOLDER'] = './app/static/images'
 
 db = SQLAlchemy(app)
